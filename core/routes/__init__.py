f973e8d589d586e6b810ee9ad64e05622cde717d
Samuel Chavez <sa5m.escom@gmail.com>
2022-10-12 01:04:15 -0500

    save new user

--- core/routes/__init__.py
+++ core/routes/__init__.py
@@ -1,10 +1,12 @@
 from core.routes.product.blueprint import product_api, product_blueprint
-
+from core.routes.user.blueprint import user_api, user_blueprint
 
 blueprints = [
     product_blueprint,
+    user_blueprint,
 ]
 
 apis = [
     product_api,
+    user_api,
 ]
